95f932107e24fad2041fcf1e7706b6cb7f96028d
nyc4m <baptiste.prunot@protonmail.com>
2020-12-01 17:49:42 +0100

    optimisation

--- src/day1/day1.test.tsx
+++ src/day1/day1.test.tsx
@@ -4,17 +4,20 @@ const example = '1721\n979\n366\n299\n675\n1456\n';
 
 const intList = [1721, 979, 366, 299, 675, 1456];
 
+const sortedList = [...intList].sort((a, b) => a-b)
+
 describe('day1', () => {
   it('should parse a list of int', () => {
     expect(parseList(example)).toEqual(intList);
   });
+
   it('should match example result', () => {
-    const tuple = findPair(intList, 2020);
+    const tuple = findPair(sortedList, 2020);
     expect(tuple[0] * tuple[1]).toEqual(514579);
   });
 
   it("should find a 'threeple' i guess", () => {
-    const [one, two, three] = findTriple(intList, 2020);
+    const [one, two, three] = findTriple(sortedList, 2020);
     expect(one * two * three).toBe(241861950);
   });
 });
--- src/day1/main.ts
+++ src/day1/main.ts
@@ -5,41 +5,26 @@ type Pair = [number, number];
 
 type Triple = [number, number, number];
 
-export function findPair(numbers: number[], expected: number): Pair {
-  const memory = new Map();
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = 0; j < numbers.length; j++) {
-      const a = numbers[i];
-      const b = numbers[j];
-      const alreadyComputed = memory.get(a) === b;
-
-      if (alreadyComputed) {
-        console.log(`Found in ${(i + 1) * (j + 1)} iteration`);
-
-        return [a, b];
-      }
-      if (a + b === expected) {
-        memory.set(b, a);
-      }
-    }
+export function findPair(sortedNumbers: number[], expected: number): Pair {
+  const first = sortedNumbers[0]
+  const last = sortedNumbers[sortedNumbers.length-1]
+  const sum = first+last
+  if(sum === expected) {
+    return [first, last]
   }
-  return [0, 0];
+  if(sum > expected) {
+    return findPair(sortedNumbers.slice(0, -1), expected)
+  }
+  if(sum < expected) {
+    return findPair(sortedNumbers.slice(1), expected)
+  }
+ return [0, 0]
 }
 
 export function findTriple(numbers: number[], expected: number): Triple {
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = 1; j < numbers.length - 1; j++) {
-      for (let k = 2; k < numbers.length - 2; k++) {
-        const a = numbers[i];
-        const b = numbers[j];
-        const c = numbers[k];
-        if (a + b + c === expected) {
-          return [a, b, c];
-        }
-      }
-    }
-  }
-  return [0, 0, 0];
+  const findFoundValue = (array: number[]) => array[0] && array[1] && array[2]
+  const res = numbers.map(a => [a, ...findPair(numbers, expected-a)]).find(findFoundValue) as Triple
+  return res ? res : [0, 0, 0];
 }
 
 export function parseList(rawList: string): number[] {
@@ -49,15 +34,21 @@ export function parseList(rawList: string): number[] {
     .map((n) => parseInt(n, 10));
 }
 
+const ascSort = (a: number, b: number) => a-b
+
+export function parseListAndSort(rawList: string): number[] {
+  return parseList(rawList).sort(ascSort)
+}
+
 async function part1() {
   const list = (await fs.readFile('./src/day1/input_day1')).toString('utf8');
-  const pair = findPair(parseList(list), 2020);
+  const pair = findPair(parseListAndSort(list), 2020);
   console.log(`Pair found : ${pair[0]} ${pair[1]} : ${pair[0] * pair[1]}`);
 }
 
 async function part2() {
   const list = (await fs.readFile('./src/day1/input_day1')).toString('utf8');
-  const pair = findTriple(parseList(list), 2020);
+  const pair = findTriple(parseListAndSort(list), 2020);
   console.log(`Triple found : ${pair[0]} ${pair[1]} ${pair[2]} : ${pair[0] *pair[1] *pair[2]}`)
 }
 
