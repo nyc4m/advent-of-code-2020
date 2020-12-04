90e5eb6c6881a65ccc63487c96770322f88efb39
nyc4m <baptiste.prunot@protonmail.com>
2020-12-04 09:32:53 +0100

    solve day4 part2

--- src/day4/day4.test.ts
+++ src/day4/day4.test.ts
@@ -1,4 +1,14 @@
-import { getPassports, validatePassports } from '.'
+import {
+  getDocuments,
+  validateDocumentFields,
+  parseOfficialDocument,
+  requiredAllFields,
+  executeFullDocumentValidation,
+  isHeightOk,
+  isColorValid,
+  isEyeColorValid,
+  isPidValid,
+} from '.'
 
 const input = `
 ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
@@ -16,12 +26,115 @@ hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in
 `
 
+const validPassport = `
+pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+`
+
+const invalidPasswords = `
+eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+`
+
 describe('day4', () => {
-  it('should contain 4 passports', () => {
-    expect(getPassports(input)).toHaveLength(4)
+  describe('only require fields', () => {
+    it('should contain 4 passports', () => {
+      expect(getDocuments(input)).toHaveLength(4)
+    })
+    it('should contain 2 valid password', () => {
+      const validPasswords = validateDocumentFields(
+        getDocuments(input).map(parseOfficialDocument),
+        requiredAllFields
+      )
+      expect(validPasswords).toBe(2)
+    })
   })
-  it('should contain 2 valid password', () => {
-    const validPasswords = validatePassports(getPassports(input))
-    expect(validPasswords).toBe(2)
+  describe('validate fields', () => {
+    it('should execute validations and find valid passwords', () => {
+      const passports = getDocuments(validPassport).map(parseOfficialDocument)
+      expect(
+        validateDocumentFields(passports, executeFullDocumentValidation)
+      ).toBe(4)
+    })
+    it('should all be invalid', () => {
+      const passports = getDocuments(invalidPasswords).map(
+        parseOfficialDocument
+      )
+      expect(
+        validateDocumentFields(passports, executeFullDocumentValidation)
+      ).toBe(0)
+    })
+  })
+  describe('isHeightOk', () => {
+    it.each(['167cm', '193cm', '150cm', '59in', '76in'])(
+      'should validate %s',
+      (value) => {
+        expect(isHeightOk(value)).toBeTruthy()
+      }
+    )
+    it.each(['194cm', '149cm', '58in', '77in', '0in', '0cm'])(
+      'should not validate %s',
+      (value) => {
+        expect(isHeightOk(value)).toBeFalsy()
+      }
+    )
+  })
+  describe('isColorValid', () => {
+    it.each(['#623a2f', '#a97842', '#b6652a'])(
+      'should validate %s',
+      (value) => {
+        expect(isColorValid(value)).toBeTruthy()
+      }
+    )
+    it.each(['#6234a2f', '#A97842', '#b62a'])('should validate %s', (value) => {
+      expect(isColorValid(value)).toBeFalsy()
+    })
+  })
+  describe('isEyeColorOk', () => {
+    it.each(['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'])(
+      'should validate %s',
+      (value) => {
+        expect(isEyeColorValid(value)).toBeTruthy()
+      }
+    )
+    it('should not validate rougeSaMère', () => {
+      expect(isEyeColorValid('rougeSaMère')).toBeFalsy()
+    })
+  })
+  describe('isPidOk', () => {
+    it.each(['087499704', '896056539', '545766238', '093154719'])(
+      'should validate %s',
+      (value) => {
+        expect(isPidValid(value)).toBeTruthy()
+      }
+    )
+
+    it.each(['08749970', '8960565399', '545AAA238', 'abcdefgha'])(
+      'should not validate %s',
+      (value) => {
+        expect(isPidValid(value)).toBeFalsy()
+      }
+    )
   })
 })
--- src/day4/index.ts
+++ src/day4/index.ts
@@ -2,36 +2,129 @@ import { readFileAsString } from '../utils'
 
 const REQUIRED_FIELDS = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
 
-export function getPassports(input: string): Array<string> {
+export interface OfficialDocument {
+  byr?: string
+  iyr?: string
+  eyr?: string
+  hgt?: string
+  hcl?: string
+  ecl?: string
+  pid?: string
+}
+
+function parseField(fieldValue: string): { name: string; value: string } {
+  const splitted = fieldValue.split(':')
+  return { name: splitted[0], value: splitted[1] }
+}
+
+export function parseOfficialDocument(
+  document: string
+): OfficialDocument {
+  return document
+    .split(/\n| /)
+    .map(parseField)
+    .reduce(
+      (obj, field) => ({
+        ...obj,
+        [field.name]: field.value,
+      }),
+      {}
+    )
+}
+
+export function getDocuments(input: string): Array<string> {
   return input.split('\n\n')
 }
 
-export function validatePassports(
-  passports: Array<string>,
+export function validateDocumentFields(
+  documents: OfficialDocument[],
+  validation: (document: OfficialDocument) => boolean,
   validPasswords: number = 0
 ): number {
-  const [passport, ...restOfPassports] = passports
-  const valid = REQUIRED_FIELDS.map((required) =>
-    passport.includes(required)
-  ).includes(false)
-    ? 0
-    : 1
-  if (passports.length === 1) {
+  const [document, ...restOfDocuments] = documents
+  const valid = validation(document) ? 1 : 0
+  if (documents.length === 1) {
     return valid + validPasswords
   }
-  return validatePassports(restOfPassports, valid + validPasswords)
+  return validateDocumentFields(
+    restOfDocuments,
+    validation,
+    valid + validPasswords
+  )
+}
+
+export function executeFullDocumentValidation(
+  document: OfficialDocument
+): boolean {
+  if (!requiredAllFields(document)) {
+    return false
+  }
+  const validations = [
+    isBetweenInclusive(parseInt(document.byr!), 1920, 2002),
+    isBetweenInclusive(parseInt(document.iyr!), 2010, 2020),
+    isBetweenInclusive(parseInt(document.eyr!), 2010, 2030),
+    isHeightOk(document.hgt!),
+    isColorValid(document.hcl!),
+    isEyeColorValid(document.ecl!),
+    isPidValid(document.pid!),
+  ]
+
+  return !validations.includes(false)
+}
+
+export const requiredAllFields = (document: OfficialDocument): boolean =>
+  !REQUIRED_FIELDS.map((requiredField) =>
+    Object.keys(document).includes(requiredField)
+  ).includes(false)
+const isBetweenInclusive = (
+  value: number,
+  min: number,
+  max: number
+): boolean => {
+  const ok = value >= min && value <= max
+  return ok
+}
+
+export const isHeightOk = (value: string): boolean => {
+  if (!/cm|in/.test(value)) {
+    return false
+  }
+  const height = parseInt(value.substr(0, value.length - 2), 10)
+  if (/cm/.test(value)) {
+    return isBetweenInclusive(height, 150, 193)
+  }
+  return isBetweenInclusive(height, 59, 76)
+}
+
+export const isColorValid = (value: string): boolean => {
+  return /^#([0-9]|[a-f]){6}$/.test(value)
+}
+
+export const isEyeColorValid = (value: string): boolean =>
+  ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(value)
+
+export const isPidValid = (value: string): boolean => {
+  return /^\d{9}$/.test(value)
 }
 
 async function part1() {
   const input = readFileAsString('./src/day4/input_day4')
-  const passports = getPassports(await input)
-  const validPasswords = validatePassports(passports)
+  const documents = getDocuments(await input).map(parseOfficialDocument)
+  const validPasswords = validateDocumentFields(documents, requiredAllFields)
+  console.log(`valid passports : ${validPasswords}`)
+}
+
+async function part2() {
+  const input = readFileAsString('./src/day4/input_day4')
+  const documents = getDocuments(await input).map(parseOfficialDocument)
+  const validPasswords = validateDocumentFields(
+    documents,
+    executeFullDocumentValidation
+  )
   console.log(`valid passports : ${validPasswords}`)
 }
 
 export const day4 = {
   part1,
-  part2: () => {
-    throw new Error('TODO')
-  },
+  part2,
 }
