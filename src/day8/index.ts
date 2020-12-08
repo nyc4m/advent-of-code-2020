7ded13758e4950e503b8b1ded65af3bea3206e2a
nyc4m <baptiste.prunot@protonmail.com>
2020-12-08 14:07:13 +0100

    solve day8 part2

--- src/day8/index.ts
+++ src/day8/index.ts
@@ -1,4 +1,3 @@
-import { isParenthesizedTypeNode } from 'typescript'
 import { readFileAsString } from '../utils'
 
 type OpType = 'acc' | 'jmp' | 'nop'
@@ -19,9 +18,13 @@ export function parseOperations(input: string): Operation[] {
 }
 
 export class Computer {
-  private readonly alreadyExecuted = new Set<number>()
+  private alreadyExecuted = new Set<number>()
   private counter = 0
   constructor(private operations: Operation[]) {}
+  reset() {
+    this.counter = 0
+    this.alreadyExecuted = new Set<number>()
+  }
   runFromPosition(
     start: number,
     lastOp: number = 0
@@ -36,6 +39,7 @@ export class Computer {
     }
     this.alreadyExecuted.add(start)
     if (this.operations.length === 0) return { res: this.counter, loop: false }
+    if(!this.operations[start]) return {res: this.counter, loop: false}
     const { type, value } = this.operations[start]
     switch (type) {
       case 'acc':
@@ -47,6 +51,27 @@ export class Computer {
         return this.runFromPosition(start + 1, start)
     }
   }
+  get suspectLines(): Operation[] {
+    return this.operations.filter(
+      (op) => op.type === 'jmp' || op.type === 'nop'
+    )
+  }
+}
+
+function fixError(
+  computer: Computer,
+  suspectedLines: Operation[]
+): { res: number; loop: boolean } {
+  const [line] = suspectedLines
+  const switchLineType = (line: string) =>line === 'nop' ? 'jmp' : 'nop'
+  line.type = switchLineType(line.type)
+  const execution = computer.runFromPosition(0)
+  if (execution.loop) {
+    computer.reset()
+    line.type = switchLineType(line.type)
+    return fixError(computer, suspectedLines.slice(1))
+  }
+  return execution
 }
 
 async function part1() {
@@ -56,9 +81,15 @@ async function part1() {
   console.log(`counter is ${computer.runFromPosition(0)}`)
 }
 
+async function part2() {
+  const input = readFileAsString('./src/day8/input_day8')
+  const operations = parseOperations(await input)
+  const computer = new Computer(operations)
+  fixError(computer, computer.suspectLines)
+  console.log(`counter is ${computer.runFromPosition(0).res}`)
+}
+
 export const day8 = {
   part1,
-  part2: () => {
-    throw new Error('TODO')
-  },
+  part2,
 }
