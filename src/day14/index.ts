8fc1d150f02107d1d06d7b2b0171c618ed40ea6b
nyc4m <baptiste.prunot@protonmail.com>
2020-12-14 11:49:29 +0100

    solve day14 part1

--- src/day14/index.ts
+++ src/day14/index.ts
@@ -11,29 +11,54 @@ export function executeInstructions(
     return memory
   }
   if (/mask/.test(instruction)) {
-    const match = instruction.match(/= (?<mask>\d*)/)
-    if (!match?.groups?.mask) throw new Error('No value for mask')
-    console.log(`mask: ${match.groups.mask}`);
-    
+    const match = instruction.match(/= (?<mask>[01X]*)/)
+    const {mask: foundMask} = match?.groups || {}
+
     return executeInstructions(
       instructions.slice(1),
-      match?.groups.mask,
+      foundMask,
       memory
     )
   }
   if (/mem/.test(instruction)) {
-    const match = instruction.match(/mem\[(?<address>\d*)\]=(?<value>\d*)/)
+    const match = instruction.match(/mem\[(?<address>\d*)\] = (?<value>\d*)/)
     const { address, value } = match?.groups || {}
-    console.log(`${address} : ${value}`);
-    
+    const maskedValue = applyMask(
+      mask,
+      leftPad(parseInt(value).toString(2), mask.length)
+    )
+    return executeInstructions(
+      instructions.slice(1),
+      mask,
+      memory.set(address, parseInt(maskedValue, 2))
+    )
   }
 
   return memory
 }
 
+function leftPad(value: string, length: number): string {
+  if (value.length === length) {
+    return value
+  }
+  return leftPad(`0${value}`, length)
+}
+
+function applyMask(mask: string, value: string): string {
+  if (!(mask.length || value.length)) {
+    return ''
+  }
+  const masked = mask[0] === 'X' ? value[0] : mask[0] === '1' ? 1 : 0
+  return masked + applyMask(mask.slice(1), value.slice(1))
+}
+
 async function part1() {
-    const instructions = (await readFileAsString('./src/day14/input_day14')).split('\n')
-    const memory = executeInstructions(List(instructions), "", Map())
+  const instructions = (
+    await readFileAsString('./src/day14/input_day14')
+  ).split('\n')
+  const memory = executeInstructions(List(instructions), '', Map())
+  console.log(memory.reduce((acc, v) => v+acc,0));
+  
 }
 
 async function part2() {
